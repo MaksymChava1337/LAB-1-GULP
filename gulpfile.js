0e4fb54b21d35a06e671a03e971f1d32dba5b641
MaksymChava1337 <maksymchava@gmail.com>
2025-10-06 05:03:38 +0300

    sorry

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,11 @@
 
-
-
 let gulp = require('gulp');
 let rename = require('gulp-rename');
 let sass = require('gulp-sass')(require('sass'));
 let cleancss = require('gulp-clean-css');
 let uglify = require('gulp-uglify');
+
+
 
 // тут крч цсс мініфікується + копіюється + добавляю суфікс мін
 function copyScss(done){
@@ -16,7 +16,7 @@ function copyScss(done){
     }))
     .on('error', console.error.bind(console))
     .pipe(cleancss())
-    .pipe(rename({suffix: '.min'}))
+    //.pipe(rename({suffix: '.min'}))
     .pipe( gulp.dest('./dist/css') );
 
  done();
@@ -32,6 +32,17 @@ function copyJs(done){
 }
 
 
+import gulp from 'gulp';
+import imagemin from 'gulp-imagemin';
+
+export default () => (
+	gulp.src('src/image/*')
+		.pipe(imagemin())
+		.pipe(gulp.dest('dist/image'))
+);
+
+
 gulp.task(copyScss);
 gulp.task(copyJs);
+
 
